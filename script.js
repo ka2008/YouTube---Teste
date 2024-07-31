8cb7ab85d1ae5f0af94181c5e4879d2f2c39b262
Kawan Rodrigues <kawanemboaba10@gmail.com>
2024-07-31 12:13:01 -0300

    Primeira atualização

---
+++ script.js
@@ -0,0 +1,6 @@
+// Este script pode ser usado para adicionar interatividade futuramente.
+document.addEventListener("DOMContentLoaded", function() {
+    // Exemplo: trocar o nome do canal ou a descrição dinamicamente
+    document.getElementById("channelName").innerText = "Meu Canal";
+    document.getElementById("channelDescription").innerText = "Bem-vindo ao meu canal no YouTube!";
+});
